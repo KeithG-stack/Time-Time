9549cfea0a71411b9968e30f780b877ac086591c
Keith <kgill0013@launchpadphilly.org>
2025-02-20 13:46:56 -0500

    new

--- src/timer/TimerDisplay.jsx
+++ src/timer/TimerDisplay.jsx
@@ -10,6 +10,8 @@ const TimerDisplay = ({ title }) => {
     const [time, setTime] = useState(25 * 60);
     // State to keep track of whether the timer is running
     const [isRunning, setIsRunning] = useState(false);
+    // want to keep track of custom time input
+    const [customTime, setCostumeTime] = useState(25);
     // Custom hook to determine if dark mode is enabled
     const isDarkMode = useDarkMode();
 
@@ -18,7 +20,12 @@ const TimerDisplay = ({ title }) => {
         if (isRunning) {
             // If the timer is running, set an interval to update the time every second
             interval = setInterval(() => {
-                setTime(prevTime => prevTime - 1);
+                setTime(prevTime => {
+                    if (prevTime === 1); {
+                        playsound();
+                    }
+                    return prevTime - 1;
+                });
             }, 1000);
         } else if (!isRunning && time !== 0) {
             // If the timer is not running and time is not zero, clear the interval
@@ -40,13 +47,32 @@ const TimerDisplay = ({ title }) => {
 
     // Function to handle the reset of the timer
     const handleReset = () => {
-        setTime(25 * 60);
+        setTime(customTime * 60);
         setIsRunning(false);
+    };
+
+    // want this to handle the custom time change
+    const handleCustomTimeChange = (e) => {
+        setCostumeTime(e.target.value);
+    };
+
+    // this is the sound that will play when the timer is over
+    const playsound = () => {
+        const audio = new Audio('/alarm.mp3');
+        audio.play();
     };
 
     return (
         <div className={`${styles.timerDisplay} ${isDarkMode ? 'dark-mode' : 'light-mode'}`} role="timer" aria-live="polite">
             <h1>{title}</h1>
+            {/* Input for custom time */}
+            <input
+                type="number"
+                value={customTime}
+                onChange={handleCustomTimeChange}
+                min="1"
+                className={styles.customTimeInput}
+                />
             {/* Display the timer value in MM:SS format */}
             <p className={styles.time}>{new Date(time * 1000).toISOString().substr(14, 5)}</p>
             <StartStopButton isRunning={isRunning} onStart={handleStart} onStop={handleStop} /> {/* Display the StartStopButton component */}
